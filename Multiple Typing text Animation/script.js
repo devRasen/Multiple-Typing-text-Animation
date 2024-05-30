44bb7268709216e4f45d6d4c9ebb1898cbb3c1d8
Md Al Amin Hossen <87903038+MdRasen@users.noreply.github.com>
2024-05-30 13:17:19 +0600

    Add files via upload

---
+++ Multiple Typing text Animation/script.js
@@ -0,0 +1,11 @@
+// Project Name: Multiple Typing text Animation
+// Author: devRasen
+// Github: https://github.com/devRasen
+
+// Typing Animation
+var typed = new Typed(".typing", {
+  strings: ["Web Developer", "YouTuber", "Graphic Designer", "Content Creator"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
